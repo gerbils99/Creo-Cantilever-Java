fe0d772586fad8a09333e0473ea935022a30bc4c
gerbils99 <george.bowles@wickens.co.uk>
2024-06-20 09:56:55 +0100

    Add files via upload

---
+++ Java/Wickens2014_C4.bat
@@ -0,0 +1,5 @@
+remset CLASSPATH=C:\PROGRA~1\PTC\CREO2~1.0\COMMON~1\M200\text\java\pfcasync.jar;.;%CLASSPATH%
+set CLASSPATH=.;%CLASSPATH%
+set PATH=C:\Program Files\PTC\Creo 4.0\M070\Common Files\x86e_win64\lib;%PATH%
+set PRO_COMM_MSG_EXE=C:\Program Files\PTC\Creo 4.0\M070\Common Files\x86e_win64\obj\pro_comm_msg.exe
+"C:\Program Files\Java\jdk-14.0.1\bin\java" -jar Wickens.jar -e
